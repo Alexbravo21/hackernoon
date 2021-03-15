e822c2a37e8160f84baac616fd74328b6811c074
Alejandro Bravo <alejandrob@spacebarmedia.com>
2021-03-15 17:52:27 -0600

    separate-components - Created Related component so clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getPosts } from './services/posts';
-import './hackernoon.scss'
+import Related from './components/Related';
+
+import './hackernoon.scss';
 
 const hackernoon_url = 'https://hackernoon.com/';
 
@@ -51,52 +53,7 @@ const App = () => {
                 <div className="reactions"></div>
             </div>
             {/* TODO: Separate related into a component */}
-            <div className="related-posts">
-                <h4 className="line-title">RELATED</h4>
-                <div className="related-posts-cont">
-                    {related.map((rel, index) => {
-                        if(rel !== undefined){
-                            return (
-                                <article className="related-post" key={index}>
-                                    {rel.type !== undefined && rel.type === 'tag' ? 
-                                    <div className="related-post-inner">
-                                        <p className="related-post-title"><a href={rel.link} className="green-link">{rel.text}</a></p>
-                                        <a href={rel.link}><img className="related-image" src={rel.image} alt=""/></a>
-                                        <div className="rel-meta">
-                                            <div className="meta-desc">
-                                                <p>{rel.companyName}</p>
-                                            </div>
-                                            <div className="time"></div>
-                                        </div>
-                                    </div> : 
-                                    <div className="related-post-inner">
-                                        <p className="related-post-title"><a href={`${hackernoon_url}${rel.slug}`} className="green-link">{rel.title}</a></p>
-                                        <a href={`${hackernoon_url}${rel.slug}`}><img className="related-image" src={rel.mainImage} alt=""/></a>
-                                        <div className="rel-meta">
-                                            <div className="meta-desc">
-                                                <img src={rel.profile.avatar} alt={rel.profile.handle} width="50" height="50" />
-                                                <div className="rel-meta-profile">
-                                                    <p className="green-link-cont"><a className="green-link" href={`${hackernoon_url}u/${profile.handle}`}>{`@${rel.profile.handle}`}</a></p>
-                                                    <p>{rel.profile.displayName}</p>
-                                                </div>
-                                            </div>
-                                            <div className="time">
-                                                <p className="estimated-time">
-                                                    {rel.estimatedTime !== undefined && `${rel.estimatedTime}min`}
-                                                </p>
-                                                <p className="rel-date">
-                                                    {new Date(rel.publishedAt*1000).toLocaleDateString()}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>}
-                                </article>
-                            );
-                        }
-                        return ''
-                    })}
-                </div>
-            </div>
+            <Related related={related} hackernoon_url={hackernoon_url} profile={profile} />
             <div className="tags">
                 <h4 className="line-title">Tags</h4>
                 <div className="tags-cont">
