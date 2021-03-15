892832acfd0d035fc523beaf6c6bdd3ed301948a
Alejandro Bravo <alejandrob@spacebarmedia.com>
2021-03-14 23:30:38 -0600

    Set json server to get the json file in an env closer to reality Created the service to obtain this data usin fetch Use hooks to parse the data and render it Created sass to start styling the markup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
+import { getPosts } from './services/posts'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Hackernoon</h1>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([]);
+    
+    useEffect(() => {
+        const Posts = async () => {
+            const posts = await getPosts();
+            setPosts(posts);
+        }
+        Posts();
+    }, [])
+
+    return (
+        <div className="App">
+        <h1>{posts.title}</h1>
+            {/* This can be sanitized but as this come from an internal API I thought it may be innecesary to install another library just for that and not make this test mini app bigger*/}
+            <div dangerouslySetInnerHTML={{__html: posts.markup}}></div>
+        </div>
+    );
 }
 
 export default App;
