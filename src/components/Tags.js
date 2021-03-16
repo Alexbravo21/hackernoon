327478be6c8819080920894f6527ad61ca277f27
Alejandro Bravo <alejandrob@spacebarmedia.com>
2021-03-15 20:00:48 -0600

    Added keys to the tags to avoid warning and added a few media queries to be more responsive

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -3,8 +3,8 @@ const Tags = ( { posts, hackernoon_url } ) => {
         <div className="tags">
             <h4 className="line-title">Tags</h4>
             <div className="tags-cont">
-                {posts.tags !== undefined && posts.tags.map((tag) => {
-                    return <a href={`${hackernoon_url}tagged/${tag}`}>{tag}</a>
+                {posts.tags !== undefined && posts.tags.map((tag, index) => {
+                    return <a href={`${hackernoon_url}tagged/${tag}`} key={index}>{tag}</a>
                 })}
             </div>
         </div>
