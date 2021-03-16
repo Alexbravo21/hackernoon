80e20fff529c5e0b5d0f0fade13f947281bb5c5e
Alejandro Bravo <alejandrob@spacebarmedia.com>
2021-03-15 18:09:29 -0600

    Separated in components every piece of the UI I thought it would be better for reuse and also to clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getPosts } from './services/posts';
+import Author from './components/Author';
 import Related from './components/Related';
+import Tags from './components/Tags';
 
 import './hackernoon.scss';
 
@@ -33,35 +35,14 @@ const App = () => {
                 <p>{date}</p>
             </div>
             <img className="post-image" src={posts.mainImage} alt=""/>
-            {/* This can be sanitized but as this come from an internal API I thought it may be innecesary to install another library just for that and not make this test mini app bigger*/}
             <div className="post-body grid-container">
-                <div className="author-profile">
-                    <img src={profile.avatar} alt="" width="50px" height="50px" className="author-profile-img" />
-                    <p className="green-link-cont author-profile-at"><a href={`${hackernoon_url}u/${profile.handle}`} className="green-link">@{profile.handle}</a></p>
-                    <p className="author-profile-name">{profile.displayName}</p>
-                    <p className="author-profile-bio">{profile.bio}</p>
-                    <div className="profile-social">
-                        <a href={`https://twitter.com/${profile.twitter}`}>
-                            <img className="authro-profile-social-icon" src="https://hackernoon.com/social-icons/twitter-new.png" width="20" height="20" alt="twitter"/>
-                        </a>
-                        <a href={`https://github.com/${profile.github}`}>
-                            <img className="authro-profile-social-icon" src="https://hackernoon.com/social-icons/github-new.png" width="20" height="20" alt="github"/>
-                        </a>
-                    </div>
-                </div>
+            <Author hackernoon_url={hackernoon_url} profile={profile} />
+                {/* This can be sanitized but as this come from an internal API I thought it may be innecesary to install another library*/}
                 <div className="post-content" dangerouslySetInnerHTML={{__html: posts.markup}}></div>
                 <div className="reactions"></div>
             </div>
-            {/* TODO: Separate related into a component */}
             <Related related={related} hackernoon_url={hackernoon_url} profile={profile} />
-            <div className="tags">
-                <h4 className="line-title">Tags</h4>
-                <div className="tags-cont">
-                    {posts.tags !== undefined && posts.tags.map((tag) => {
-                        return <a href={`${hackernoon_url}tagged/${tag}`}>{tag}</a>
-                    })}
-                </div>
-            </div>
+            <Tags posts={posts} hackernoon_url={hackernoon_url} />
         </div>
     );
 }
